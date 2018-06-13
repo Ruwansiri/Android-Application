5d512e03fbd21317c40707b9312f972223c9aed4
Ruwansiri <ruwansriprotocol@gmail.com>
2018-06-13 09:41:55 -0700

    Add files via upload

---
+++ javatest.java
@@ -0,0 +1,25 @@
+int p=-1;
+int arr [] = new int [5];
+
+public void push int (val);
+if(!isfull){
+	p++;
+	arr [p]=val;
+}
+else{
+		System.out.println("Stack Overflow");
+	}
+}
+
+
+
+public int pop(){
+	int tmp= or [p];
+	if(!isfull){
+		p--;
+		return tmp;
+	}
+	else{
+		System.out.println("Stack Underflow");
+	}
+}
